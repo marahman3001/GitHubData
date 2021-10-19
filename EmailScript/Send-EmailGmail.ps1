cc785f297a77323929aad5ed4dc53b742e55f752
marahman3001 <abdulrahman3001@yahoo.com>
2021-10-19 17:46:15 +0100

    init

---
+++ EmailScript/Send-EmailGmail.ps1
@@ -0,0 +1,17 @@
+﻿# Send Email via Gmail   (make sure you turn on less secure apps  https://myaccount.google.com/lesssecureapps)
+# --------------------------
+$From = "icon3001@gmail.com"
+$Password = 'M3$aghar_128'
+ $To = "abdulrahman3001@yahoo.com"
+ $Cc = "md_abdul_rahman3001@yahoo.com"
+ $Subject = "Email Subject with attachment 3"
+ $Body =  gc "H:\temp\Newfile.txt" | out-string  # "Insert body text here"
+ $SMTPServer = "smtp.gmail.com"
+ $SMTPPort = "587"
+ $attachment = "H:\temp\securityLog.csv"
+
+ $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($From, $secpasswd)
+
+
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $mycreds -Attachments $Attachment
